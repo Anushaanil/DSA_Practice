d69736df7d0464b7f7e15da6f149d49e95e30b7b
AnushaAnil <anushaanil44@gmail.com>
2026-03-04 23:37:05 +0530

    trees practice

--- Binary Search/practice.py
+++ Binary Search/practice.py
@@ -1,22 +1,113 @@
-def isBadVersion(mid, bad=4):
-     return mid >= bad
+def binary_search(nums, target: int) -> int:
+        left = 0
+        right = len(nums)-1
+
+        while left<=right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid]<target:
+                left = mid+1
+            else:
+                right = mid-1
+
+        return -1
+
+nums = [1,2,3,4,5]
+target = 3
+# print(binary_search(nums, target))
+
+def searchInsert(nums, target):
+    left = 0
+    right = len(nums)-1
+
+    while left<=right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid]<target:
+            left = mid+1
+            
+        else:
+            right = mid-1
+
+    return left
+
+nums = [1,2,4,5,6]
+target = 3
+# print(searchInsert(nums, target))
+
+
+def find_target_in_infinite_array(arr, target):
+    # as arr can be infinite we donno the len, so we need to start from starting indices
+    # use bottom up approach by doubling the search len and if target is found in the given 
+    # range then apply binary search
+    s = 0
+    e = 1
+
+    while target > arr[e]:
+        temp = e + 1
+        e = e + (e-s+1) * 2
+        s = temp
+
+    while s<=e:
+        m = (s+e)//2
+        print('here', m, arr[m])
+
+        if arr[m] == target:
+            return m
+        elif arr[m] < target:
+            s = m+1
+        else:
+            e = m-1
+
+    return -1
+
+arr = [2,3,4,5,6,7,8,10,11,12,15,20,23,50]
+target = 15
+# print(find_target_in_infinite_array(arr, target))
+
+
+def nextGreatestLetter(letters, target):
+    l = 0
+    r = len(letters)-1
+
+    while l<r:
+        mid = (l+r)//2
+        if letters[mid] > target:
+            r = mid
+        else:
+            l = mid+1
+
+    return letters[l] if letters[l]>target else letters[0]
+
+letters = ["x","x","f","j"]
+target = "z"
+# print(nextGreatestLetter(letters, target))
+
      
 def firstBadVersion(n):
-        l = 1
-        r = n
+    def isBadVersion(mid, bad=4):
+        return mid >= bad
+    
+    l = 1
+    r = n
 
-        while l<r:
-            mid = (l+r)//2
-            print(l,r)
+    while l<r:
+        mid = (l+r)//2
+        print(l,r)
 
-            if isBadVersion(mid):
-                r = mid
-            else:
-                 l = mid+1
-                
-        return l
+        if isBadVersion(mid):
+            r = mid
+        else:
+                l = mid+1
+            
+    return l
 
 # print(firstBadVersion(5))
+
 
 class Solution:
     def find_1st_occurence(self, l, r, nums, target):
@@ -45,7 +136,8 @@ class Solution:
                 r = mid-1
 
         return r if nums[r] == target else -1
-
+    
+    # 1st and last position of ele in array
     def searchRange(self, nums, target):
         if not nums:
             return [-1, -1]
@@ -58,11 +150,14 @@ class Solution:
 # nums = [5, 6, 7, 7, 7, 8]
 # target = 6
 # print(s.searchRange(nums, target))
-    
+
+
 def search_in_rotated_sorted_array(nums, target):
     l = 0
     r = len(nums)-1
-
+    # another approach find pivot i.e peak element
+    # search in left side of peak
+    # if not found then search in right side of peak
     while l<=r:
         mid = (l+r)//2
         if nums[mid] == target:
@@ -84,6 +179,7 @@ def search_in_rotated_sorted_array(nums, target):
 # target = 6
 # print(search_in_rotated_sorted_array(nums, target))
 
+
 def min_in_rotated_sorted_array(nums):
     l = 0
     r = len(nums)-1
@@ -97,5 +193,175 @@ def min_in_rotated_sorted_array(nums):
 
     return nums[l]
 
-nums = [11,15,20,23]
-print(min_in_rotated_sorted_array(nums))
+# nums = [11,15,20,23]
+# print(min_in_rotated_sorted_array(nums))
+
+
+def findPeakElement(nums) -> int:
+        l = 0
+        r = len(nums)-1
+
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid] > nums[mid+1]:
+                r = mid
+            else:
+                l = mid+1
+
+        return l
+
+nums = [1,2,3,4,5,3,1,0]
+# print(findPeakElement(nums))
+
+
+def mySqrt(x: int) -> int:
+    l = 1
+    r = x
+
+    if x == 0:
+        return 0
+
+    while l<r:
+        mid = (l+r+1)//2
+        if mid * mid <= x:
+            l = mid
+        else:
+            r = mid-1
+
+    return l
+
+# print(mySqrt(8))
+
+
+import math
+def minEatingSpeed(piles, h):
+    l = 1
+    r = max(piles)
+
+    while l < r:
+        mid = (l+r)//2
+        # math.ceil(i/k)
+        total_hours = sum((i+mid-1)//mid for i in piles)
+        if total_hours <= h:
+            r = mid
+        else:
+            l = mid+1
+    return l
+
+# piles = [30,11,23,4,20]
+# h = 6
+# print(minEatingSpeed(piles, h))
+
+
+def shipWithinDays(weights, days):
+    l = max(weights)
+    r = sum(weights)
+
+    while l<r:
+        mid = (l+r)//2
+        total_days = 1
+        total_weight = 0
+
+        for weight in weights:
+            if total_weight + weight <= mid:
+                total_weight+=weight
+            else:
+                total_days+=1
+                total_weight = weight
+
+        if total_days <=days:
+            r = mid
+        else:
+            l = mid+1
+    return l
+    
+weights = [1,2,3,4,5,6,7,8,9,10]
+days = 5
+# print(shipWithinDays(weights, days))
+
+
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+class MountainArray:
+   def __init__(self, arr, target):
+       self.mountainArr = arr
+       self.target = target
+
+   def get(self, index: int) -> int:
+       return self.mountainArr[index]
+   
+   def length(self) -> int:
+       return len(self.mountainArr)
+
+class Solution:
+    def findPeak(self, mountainArr):
+        l = 0
+        r = mountainArr.length() - 1
+
+        while l<r:
+            m = (l+r)//2
+            if mountainArr.get(m) >= mountainArr.get(m+1):
+                r = m
+            else:
+                l = m + 1
+        return l
+
+    def binarySearch(self, mountainArr, l, r, target, ans, is_asc):
+        while l<=r:
+            m = (l+r)//2
+            if mountainArr.get(m) == target:
+                ans = m
+                return ans
+            
+            if is_asc:
+                if mountainArr.get(m) > target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                print(mountainArr.get(m), target)
+                if mountainArr.get(m) > target:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return ans
+
+    def findInMountainArray(self, target: int, mountainArr):
+        # find peak
+        peak = self.findPeak(mountainArr)
+        print('peak', peak)
+
+        # search in left side of the peak
+        default_ans = -1
+        ans = self.binarySearch(mountainArr, 0, peak, target, default_ans, True)
+        print('left side ans', 0, peak, ans)
+        if ans==-1:
+            ans = self.binarySearch(mountainArr, peak+1, mountainArr.length()-1, target, default_ans, False)
+            print('right side ans', peak+1, mountainArr.length()-1, ans)
+        return ans
+
+arr = [1,2,3,4,5,3,1,0]
+target = 8
+m = MountainArray(arr, target)
+s = Solution()
+# print(s.findInMountainArray(m.target, m))
+
+
+def find_rotation_count_in_rotated_sorted_array(arr):
+    l = 0
+    r = len(arr)-1
+
+    while l<r:
+        m = (l+r)//2
+        print(l, m, r)
+        if arr[m] < arr[r]:
+            r = m
+        else:
+            l = m+1
+    return l
+
+# count of rotations = index of min element
+arr = [1,2,3,4,5,6]
+print(find_rotation_count_in_rotated_sorted_array(arr))
---
+++ Trees/practice.py
@@ -0,0 +1,170 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+root = TreeNode(1)
+
+l1 = TreeNode(2)
+l2 = TreeNode(4)
+l3 = TreeNode(5)
+l4 = TreeNode(7)
+
+r1 = TreeNode(3)
+r2 = TreeNode(6)
+r3 = TreeNode(8)
+r4 = TreeNode(9)
+
+root.left = l1
+root.right = r1
+l1.left = l2
+r1.left = l3
+r1.right = r2
+r2.left = l4
+r2.right = r3
+r3.right = r4
+
+
+class TreeSolution:
+    def printTree(self, root):
+        if not root:
+            return None
+        
+        print('root', root.val)
+
+        if root.left:
+            print('left', root.left.val)
+        else:
+            print('left', None)
+        
+        if root.right:
+            print('right', root.right.val)
+        else:
+            print('right', None)
+        
+        self.printTree(root.left)
+        self.printTree(root.right)
+
+        return 
+    
+    def maxDepth(self, root) -> int:
+        if not root:
+            return 0
+
+        if root.left:
+            print('left', root.left.val)
+        else:
+            print('left', None)
+
+        left_depth = self.maxDepth(root.left)
+
+        if root.right:
+            print('right', root.right.val)
+        else:
+            print('right', None)
+        
+        right_depth = self.maxDepth(root.right)
+
+        print('depth', left_depth, right_depth)
+        print('sol', 1 + max(left_depth, right_depth))
+        return 1 + max(left_depth, right_depth)
+    
+    def leafCount(self, root):
+        # Base case 
+        if root is None:
+            return 0
+
+        # conditional case for leaf
+        if root.left is None and root.right is None:
+            return 1
+        
+        l_nodes_count = self.leafCount(root.left)
+        r_nodes_count = self.leafCount(root.right)
+        
+        return l_nodes_count + r_nodes_count
+    
+    def isSameTree(self, p, q) -> bool:
+        if p is None and q is None:
+            return True
+        
+        if p is None or q is None:
+            return False
+        
+        if p.val!=q.val:
+            return False
+        
+        left_check = self.isSameTree(p.left, q.left)
+        right_check = self.isSameTree(p.right, q.right)
+
+        return left_check and right_check
+            
+    def invert_binary_tree(self, root):
+        if not root:
+            return None
+        
+        # temp = root.left
+        # root.left = root.right
+        # root.right = temp
+
+        root.left, root.right = root.right, root.left
+
+        self.invert_binary_tree(root.left)
+        self.invert_binary_tree(root.right)
+
+        return root
+    
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        
+        if left is None or right is None:
+            return False
+        
+        if left.val != right.val:
+            return False
+        
+        return self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
+    
+    def isSymmetric(self, root) -> bool:
+        if not root:
+            return True
+
+        return self.isMirror(root.left, root.right)
+    
+t = TreeSolution()
+# print(t.maxDepth(root))
+# print(t.leafCount(root))
+p = TreeNode(1)
+p1 = TreeNode(2)
+# p2 = TreeNode(3)
+p.left = p1
+
+q = TreeNode(1)
+q1 = TreeNode(2)
+q.right = q1
+
+# print(t.isSameTree(p, q))
+# t.printTree(root)
+# print("\n")
+# res = t.invert_binary_tree(root)
+# t.printTree(res)
+
+p = TreeNode(1)
+l1 = TreeNode(2)
+r1 = TreeNode(2)
+
+l2 = TreeNode(3)
+r2 = TreeNode(4)
+
+l3 = TreeNode(4)
+r3 = TreeNode(3)
+p.left = l1
+p.right = r1
+l1.left = l2
+l1.right = r2
+r1.left = l3
+r1.right = l3
+
+print(t.isSymmetric(p))
