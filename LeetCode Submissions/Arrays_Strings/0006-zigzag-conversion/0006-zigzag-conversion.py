6b9a0d992fd61f6d14e6c13888880fceac94f77c
AnushaAnil <anushaanil44@gmail.com>
2026-06-14 17:15:45 +0530

    new submissions

--- LeetCode Submissions/Arrays_Strings/0006-zigzag-conversion/0006-zigzag-conversion.py
+++ LeetCode Submissions/Arrays_Strings/0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -26,3 +26,24 @@ class Solution:
                     res[r]+=s[j2]
             
         return ''.join(res)
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        current_row = 0
+        direction = 1
+
+        for char in s:
+            rows[current_row] += char
+
+            if current_row == 0:
+                direction = 1
+            elif current_row == numRows - 1:
+                direction = -1
+
+            current_row += direction
+
+        return "".join(rows)
--- LeetCode Submissions/Arrays_Strings/0014-longest-common-prefix/0014-longest-common-prefix.py
+++ LeetCode Submissions/Arrays_Strings/0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -15,3 +15,20 @@ class Solution:
             longest_str+=char
 
         return longest_str
+    
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+
+        return prefix
+
+        
